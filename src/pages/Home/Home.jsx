346d3d1a49567cb37238352960fbd17bcf6ab85f
rexaulrana <rexaul.rana@gmail.com>
2024-02-01 05:39:25 +0600

    add show details page and booking form

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import AllShow from "../../components/AllShow/AllShow";
 
 const Home = () => {
-  //   console.log(shows);
   return (
     <div className="mt-4 mb-5">
       <AllShow></AllShow>
--- src/pages/ShowDetails/ShowDetails.jsx
+++ src/pages/ShowDetails/ShowDetails.jsx
@@ -10,12 +10,32 @@ const ShowDetails = () => {
   // console.log(shows);
   useEffect(() => {
     const details = shows?.find((showItem) => showItem?.show?.id == id);
-    console.log(details);
+    // console.log(details);
     setShowDetails(details);
   }, [id, shows]);
 
   const handleBack = () => {
     window.history.back();
+  };
+
+  // handle booking
+  const handleBooking = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    // const showName = {showDetails?.show?.summary};
+    const customerName = form.name.value;
+    const date = form.date.value;
+    const number = form.number.value;
+    const hall = form.hall.value;
+    const customerInfo = {
+      showName: showDetails?.show?.name,
+      customerName,
+      date,
+      number,
+      hall,
+    };
+    console.log(customerInfo);
+    localStorage.getItem("booked-show");
   };
   return (
     <div>
@@ -27,7 +47,123 @@ const ShowDetails = () => {
           <a onClick={handleBack} className="btn btn-dark me-2">
             Go back
           </a>
-          <a className="btn btn-secondary">Book</a>
+
+          {/* booking form */}
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+          >
+            Book Now
+          </button>
+
+          <div
+            className="modal fade"
+            id="staticBackdrop"
+            data-bs-backdrop="static"
+            data-bs-keyboard="false"
+            aria-labelledby="staticBackdropLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <form
+                    onSubmit={handleBooking}
+                    className="modal-title fs-5"
+                    id="staticBackdropLabel"
+                  >
+                    {/* show name */}
+                    <div className="mb-2">
+                      <label className="form-label">Name</label>
+                      <div className="input-group">
+                        <input
+                          required
+                          value={showDetails?.show?.name}
+                          type="text"
+                          name="showName"
+                          className="form-control"
+                          id="basic-url"
+                          aria-describedby="basic-addon3 basic-addon4"
+                        />
+                      </div>
+                    </div>
+                    {/* customer name */}
+                    <div className="mb-2">
+                      <label className="form-label">Your Name</label>
+                      <div className="input-group">
+                        <input
+                          required
+                          type="text"
+                          placeholder="Type Your Name"
+                          name="name"
+                          className="form-control"
+                          id="basic-url"
+                          aria-describedby="basic-addon3 basic-addon4"
+                        />
+                      </div>
+                    </div>
+                    {/* customer phone number */}
+                    <div className="mb-2">
+                      <label className="form-label">Phone Number</label>
+                      <div className="input-group">
+                        <input
+                          required
+                          type="number"
+                          name="number"
+                          className="form-control"
+                          id="basic-url"
+                          aria-describedby="basic-addon3 basic-addon4"
+                        />
+                      </div>
+                    </div>
+                    {/* booking date */}
+                    <div className="mb-2">
+                      <label className="form-label">Select date</label>
+                      <div className="input-group">
+                        <input
+                          required
+                          type="date"
+                          name="date"
+                          className="form-control"
+                          id="basic-url"
+                          aria-describedby="basic-addon3 basic-addon4"
+                        />
+                      </div>
+                    </div>
+                    {/* Hall */}
+                    <div className="mb-2">
+                      <label className="form-label">Select Hall</label>
+                      <div className="input-group">
+                        <select
+                          name="hall"
+                          required
+                          className="form-select"
+                          aria-label="Default select example"
+                        >
+                          <option defaultValue>Open this select menu</option>
+                          <option>Hall one</option>
+                          <option>Hall Two</option>
+                          <option>Hall Three</option>
+                        </select>
+                      </div>
+                    </div>
+                    <button
+                      type="button"
+                      className="btn btn-secondary me-2"
+                      data-bs-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                    <input type="submit" className="btn btn-primary" />
+                  </form>
+                </div>
+
+                <div className="modal-footer"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
